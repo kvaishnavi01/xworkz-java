3de78f63c220164f36d7f297b568582bfa15bd73
kvaishnavi01 <kamathvaishnavi01@gmail.com>
2025-03-26 09:01:49 +0530

    Delete PrivateVariables/src/com/xworkz/privateSpecifier directory

--- PrivateVariables/src/com/xworkz/privateSpecifier/CameraType.java
+++
@@ -1,5 +0,0 @@
-package com.xworkz.privateSpecifier;
-
-public enum CameraType {
-    DSLR, MIRRORLESS, ACTION, COMPACT
-}
--- PrivateVariables/src/com/xworkz/privateSpecifier/SatelliteType.java
+++
@@ -1,5 +0,0 @@
-package com.xworkz.privateSpecifier;
-
-public enum SatelliteType {
-    COMMUNICATION, WEATHER, NAVIGATION, RESEARCH
-}
